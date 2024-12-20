cd3801edb26704b205d07894dcee8d697e6763e8
yocxy2 <161643699+yocxy2@users.noreply.github.com>
2024-12-20 15:39:31 -0800

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -78,8 +78,8 @@ function Header() {
 
   return (
     <>
-      <div className="bg-[#ffe600]">
-        <header className="w-full bg-[#ffe600] sticky top-0 z-50 px-2.5 lg:px-0 py-2 lg:py-2 lg:border-0 border-b-[0.1px] border-[#dfcf3e] ">
+      <div className="bg-[#ec0707]">
+        <header className="w-full bg-[#ec0707] sticky top-0 z-50 px-2.5 lg:px-0 py-2 lg:py-2 lg:border-0 border-b-[0.1px] border-[#dfcf3e] ">
           <div className="max-w-[1200px] mx-auto flex justify-between items-center">
             <Link href="/">
               <div className="w-24 lg:w-80 lg:mr-14 lg:-ml-1 lg:overflow-hidden">
